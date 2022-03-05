fa1003c845e863cf5db312339422b75a83701127
Pure <kirill2283371@mail.ru>
2022-03-05 21:21:21 +0300

    Add a basis for fixing two requests in one connection

--- Database.py
+++ Database.py
@@ -1,3 +1,4 @@
+import inspect
 from datetime import datetime
 
 import mysql.connector
@@ -7,7 +8,16 @@ from FunctionResult import FunctionResult
 
 def _use_one_time_connection(function_to_decorate):
     def wrapper_function(self, *params):
-        self.connect_and_set_cursors(self._parameters)
+        function_arguments = inspect.getfullargspec(function_to_decorate).args
+        if "connection" not in function_arguments:
+            raise Exception("Connection parameters isn't exists in function")
+
+        connection_index = function_arguments.index("connection")
+        if len(params) < connection_index or not isinstance(params[connection_index],
+                                                            mysql.connector.connection_cext.CMySQLConnection):
+            new_connection = self.connect_and_set_cursors(self._parameters)  # replace with connection get
+            params = params[0:connection_index] + (self.connection,) + params[connection_index:]
+
         decorated_function_result = function_to_decorate(self, *params)
         self.connection.close()
         return decorated_function_result
@@ -41,7 +51,7 @@ class Database:
     # Method what will be called if table isn't exists
     # Method must be called only from places where user cannot change something
     @_use_one_time_connection
-    def execute_custom_query(self, command, commit=False):
+    def execute_custom_query(self, command, connection, commit=False):
         print(self.cursor)
         self.cursor.execute(command)
         if commit:
@@ -50,7 +60,7 @@ class Database:
         return self.cursor.fetchall()
 
     @_use_one_time_connection
-    def add_new_user(self, telegram_id):
+    def add_new_user(self, telegram_id, connection):
         self.cursor.execute("""SELECT * FROM users WHERE telegram_id = %s""", [telegram_id])
 
         if self.cursor.fetchone() is None:
@@ -71,7 +81,7 @@ class Database:
     """
 
     @_use_one_time_connection
-    def search_blogger_in_database(self, blogger_short_name=None, blogger_id=None):
+    def search_blogger_in_database(self, connection, blogger_short_name=None, blogger_id=None):
         if blogger_short_name is None and blogger_id is None:
             raise Exception('One of arguments must be given')
 
@@ -91,14 +101,14 @@ class Database:
         return self.cursor.fetchone()
 
     @_use_one_time_connection
-    def check_is_user_subscribe_for_blogger(self, telegram_id, blogger_short_name):
+    def check_is_user_subscribe_for_blogger(self, telegram_id, blogger_short_name, connection):
         self.cursor.execute("""SELECT * FROM user_subscriptions WHERE user_id = %s AND blogger_id = %s""",
                             [telegram_id, blogger_short_name])
 
         return self.cursor.fetchone() is not None
 
     @_use_one_time_connection
-    def subscribe_user(self, telegram_id, blogger_short_name):
+    def subscribe_user(self, telegram_id, blogger_short_name, connection):
         if self.check_is_user_subscribe_for_blogger(telegram_id, blogger_short_name):
             return FunctionResult.error('Вы уже подписаны этого пользователя')
 
@@ -110,7 +120,7 @@ class Database:
 
     # blogger_data - array of blogger id, blogger short name, posts count, last post id
     @_use_one_time_connection
-    def add_new_blogger(self, blogger_data):
+    def add_new_blogger(self, blogger_data, connection):
         if self.search_blogger_in_database(blogger_id=blogger_data[0]) is not None:
             return False
 
@@ -120,13 +130,13 @@ class Database:
         return True
 
     @_use_one_time_connection
-    def add_tariff_to_user(self, tariff_id, telegram_id, started_at=datetime.now()):
+    def add_tariff_to_user(self, tariff_id, telegram_id, connection, started_at=datetime.now()):
         self.cursor.execute("""INSERT INTO user_tariffs VALUES (%s, %s, %s)""", [telegram_id, tariff_id, started_at])
 
         self.connection.commit()
 
     @_use_one_time_connection
-    def get_valid_user_tariffs(self, telegram_id):
+    def get_valid_user_tariffs(self, telegram_id, connection):
         self.cursor.execute("""SELECT * FROM user_tariffs 
                                        INNER JOIN tariffs ON tariff_id = tariffs.id 
                                        WHERE user_id = %s AND (tariffs.duration IS NULL 
@@ -138,7 +148,7 @@ class Database:
         return tariffs
 
     @_use_one_time_connection
-    def get_user_tariffs(self, telegram_id):
+    def get_user_tariffs(self, telegram_id, connection):
         self.cursor.execute("""SELECT * FROM user_tariffs 
                                INNER JOIN tariffs ON tariff_id = tariffs.id WHERE user_id = %s""",
                             [telegram_id])
@@ -148,7 +158,7 @@ class Database:
         return tariffs
 
     @_use_one_time_connection
-    def get_user_subscriptions(self, telegram_id):
+    def get_user_subscriptions(self, telegram_id, connection):
         self.cursor.execute("""SELECT user_id, CONVERT(`blogger_id`, char), bloggers.short_name, 
                                bloggers.posts_count, CONVERT(bloggers.last_post_id, char) 
                                FROM user_subscriptions 
@@ -158,7 +168,7 @@ class Database:
         return self.cursor.fetchall()
 
     @_use_one_time_connection
-    def unsubscribe_user(self, telegram_id, blogger_id):
+    def unsubscribe_user(self, telegram_id, blogger_id, connection):
         self.cursor.execute("""DELETE
                                FROM user_subscriptions
                                WHERE user_id = %s
