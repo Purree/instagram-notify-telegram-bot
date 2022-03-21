75f5681bff15a5a33d1949efbcd7390fcc7a515f
Pure <kirill2283371@mail.ru>
2022-03-21 21:32:31 +0300

    Delete deprecated methods from instagram controller.

--- InstagramController.py
+++ InstagramController.py
@@ -9,10 +9,6 @@ from Debug import Debug
 
 class InstagramController:
     BLOGGER_DATA_LINK = "https://www.instagram.com/%s/?__a=1"  # %s - blogger short name
-    BLOGGER_STORIES_DATA_LINK = "https://i.instagram.com/api/v1/feed/user/%s/reel_media/"  # %s - blogger id
-    BLOGGER_STORIES_USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (" \
-                                 "KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; " \
-                                 "en_US; en-US; scale=2.00; 828x1792; 165586599) "
 
     def __init__(self):
         self.config = Config()
@@ -20,11 +16,6 @@ class InstagramController:
         self.proxy = self.config.read_from_config("PROXY", "proxy")
         self.debug = Debug()
         setattr(asyncio.sslproto._SSLProtocolTransport, "_start_tls_compatible", True)
-
-    # TEMPORARILY FROZEN AND IDENTICAL BECAUSE USER STORIES CANNOT GET WITHOUT LOGIN
-    # I HIGHLY RECOMMEND USING get_blogger_main_info METHOD.
-    def get_blogger_info(self, blogger_short_name, blogger_id=None):
-        self.get_blogger_main_info(blogger_short_name)
 
     def get_blogger_id(self, short_name=None, blogger_data=None):
         if short_name is None and blogger_data is None:
@@ -54,18 +45,6 @@ class InstagramController:
 
     def get_main_info_of_many_bloggers(self, bloggers_short_names):
         return asyncio.run(self._get_main_info_of_many_bloggers(bloggers_short_names))
-
-    # WORK ONLY WITH LOGGED IN ACCOUNT.
-    # TEMPORARILY FROZEN
-    def get_blogger_stories_info(self, blogger_id):
-        raise Exception('TEMPORARILY FROZEN')
-        result = requests.get(self.BLOGGER_STORIES_DATA_LINK % blogger_id,
-                              headers={'User-Agent': self.BLOGGER_STORIES_USER_AGENT}, proxies=self.proxy,
-                              cookies={"sessionid": self.config.read_from_config("INSTAGRAM", "sessionid")})
-
-        print(result.text)
-
-        return result.json()
 
     def get_bloggers_with_subscriptions(self):
         return self.database.get_bloggers_with_subscriptions()
