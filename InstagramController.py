56eb8a0cab53212b8b60a6c1398663a7cb8f1abb
Pure <kirill2283371@mail.ru>
2022-03-22 21:08:15 +0300

    Return stories api link and stories user agent to Instagram controller.

--- InstagramController.py
+++ InstagramController.py
@@ -9,6 +9,8 @@ from Debug import Debug
 
 class InstagramController:
     BLOGGER_DATA_LINK = "https://www.instagram.com/%s/?__a=1"  # %s - blogger short name
+    BLOGGER_STORIES_LINK = "https://i.instagram.com/api/v1/feed/%s/1/reel_media/"
+    BLOGGER_STORIES_USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)"
 
     def __init__(self):
         self.config = Config()
